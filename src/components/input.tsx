48a9fc208d1d39da5b74cbba1af38347839f831f
Lucas Matheus Vieira Lúcio <lucmvl2003@gmail.com>
2023-02-24 16:04:23 -0300

    trabalhando no input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, TextInput } from 'react-native';
 
 export function Input() {
   return (
-    <View className="w-max pb-6 pt-6 min-h-fit bg-secondary items-center justify-between flex-row">
+    <TextInput placeholder='Digite algo...' placeholderTextColor={'#a8a8b3'} className="w-max pb-6 pt-6 min-h-fit bg-gray-500 text-white items-center justify-between flex-row">
       
-    </View>
+    </TextInput>
   );
 }
--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { Header } from '../components/header';
+import { Input } from '../components/input';
 
 export function Home() {
   return (
     <View className="flex-1 bg-dark">
       <Header />
+      <Input />
     </View>
   );
 }
