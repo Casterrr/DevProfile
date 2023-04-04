ca3325331f870252ce2fd120b6d4f814b0507c7e
Lucas Matheus Vieira Lúcio <lucasmvl2003@gmail.com>
2023-04-04 13:35:13 -0300

    Módulo 10 concluído

--- src/screens/signIn.tsx
+++ src/screens/signIn.tsx
@@ -1,22 +1,33 @@
 import React from 'react';
-import { Image, ScrollView, Text, View, TouchableOpacity } from 'react-native';
+import { Image, ScrollView, Text, View, TouchableOpacity, KeyboardAvoidingView, Platform } from 'react-native';
 import { Input } from '../components/Form/input';
 import { Button } from '../components/Form/button';
+
+import { useNavigation } from '@react-navigation/native'
 
 import { Feather } from '@expo/vector-icons'
 
 import logo from '../assets/logo.png';
 
+interface ScreenNavigationProp {
+  navigate: (screen: string) => void;
+}
+
 export function SignIn() {
+  
+  const navigation =  useNavigation<ScreenNavigationProp>();
+  
   return (
-    <>
+    <KeyboardAvoidingView enabled style={{ flex: 1}} behavior={Platform.OS === 'ios'? 'padding' : undefined}>
       <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flexGrow: 1 }}>
         <View className="flex-1 bg-dark">
       
           <View className="flex-1 items-center justify-center w-max p-6">
-      
+
             <Image className="w-40 h-40 rounded-lg mb-8" source={logo} ></Image>
-            <Text className="text-lg text-light mb-6">Faça seu login</Text>
+            <View>
+              <Text className="text-lg text-light mb-6">Faça seu login</Text>
+            </View>
       
             <Input placeholder = 'Email' className="w-full mb-4 rounded-md px-4 py-5 bg-gray800 text-light"/>
             <Input placeholder = 'Senha' className="w-full rounded-md px-4 py-5 bg-gray800 text-light"/>
@@ -30,11 +41,11 @@ export function SignIn() {
         </View>
       </ScrollView>
     
-      <TouchableOpacity className='absolute left-0 right-0 bottom-0 bg-gray800 border-t-2 border-black flex-row py-4 items-center justify-center mt-6'>
+      <TouchableOpacity onPress={() => {navigation.navigate('SignUp')}} className='absolute left-0 right-0 bottom-0 bg-gray800 border-t-2 border-black flex-row py-4 items-center justify-center mt-6'>
         <Feather name='log-in' size={28} color={'#eba417'} />
         <Text className='font-robotoRegular text-lg ml-4 text-primary'>Criar uma conta</Text>
       </TouchableOpacity>
 
-    </>
+    </KeyboardAvoidingView>
   );
 }
--- src/screens/signUp.tsx
+++ src/screens/signUp.tsx
@@ -1,26 +1,48 @@
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { Image, KeyboardAvoidingView, Platform, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import { Input } from '../components/Form/input';
 import { Button } from '../components/Form/button';
 
+import { Feather } from '@expo/vector-icons'
+
+import logo from '../assets/logo.png';
+import { useNavigation } from '@react-navigation/native';
+
+interface ScreenNavigationProp {
+  goBack: () => void;
+}
+
 export function SignUp() {
+
+  const { goBack } =  useNavigation<ScreenNavigationProp>();
+
   return (
-    <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
-      <View className="flex-1 bg-dark">
+    <KeyboardAvoidingView enabled style={{ flex: 1}} behavior={Platform.OS === 'ios'? 'padding' : undefined}>
+      <ScrollView keyboardShouldPersistTaps="handled" contentContainerStyle={{ flex: 1 }}>
+        <View className="flex-1 bg-dark">
 
-        <View className="flex-1 items-center justify-center w-max p-6">
+          <View className="flex-1 items-center justify-center w-max p-6">
 
-          <Text className="text-lg text-light mb-6">Crie sua conta</Text>
+          <Image className="w-40 h-40 rounded-lg mb-8" source={logo} ></Image>
 
-          <Input placeholder = 'Nome' className="w-full mb-4 rounded-md px-4 py-5 bg-gray800 text-light"/>
-          <Input placeholder = 'Email' className="w-full mb-4 rounded-md px-4 py-5 bg-gray800 text-light"/>
-          <Input placeholder = 'Senha' className="w-full rounded-md px-4 py-5 bg-gray800 text-light"/>
+            <Text className="text-lg text-light mb-6">Crie sua conta</Text>
 
-          <Button title={'Criar'} className="w-full mt-6 rounded-md"/>
+            <Input placeholder = 'Nome' className="w-full mb-4 rounded-md px-4 py-5 bg-gray800 text-light"/>
+            <Input placeholder = 'Email' className="w-full mb-4 rounded-md px-4 py-5 bg-gray800 text-light"/>
+            <Input placeholder = 'Senha' className="w-full rounded-md px-4 py-5 bg-gray800 text-light"/>
+
+            <Button titleStyles='' title={'Criar'} className="w-full mt-6 rounded-md"/>
+
+          </View>
 
         </View>
+      </ScrollView>
 
-      </View>
-    </ScrollView>
+      <TouchableOpacity onPress={() => goBack()} className='absolute left-0 right-0 bottom-0 bg-gray800 border-t-2 border-black flex-row py-4 items-center justify-center mt-6'>
+        <Feather name='arrow-left' size={28} color={'#eba417'} />
+        <Text className='font-robotoRegular text-lg ml-4 text-primary'>Voltar ao SignIn</Text>
+      </TouchableOpacity>
+
+    </KeyboardAvoidingView>
   );
 }
